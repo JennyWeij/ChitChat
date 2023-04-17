9cf8d60beb6d6e38b00849e30cac2f2fc05dfd5b
JennyWeij <jennyweij@outlook.com>
2023-04-17 15:56:13 +0200

    lagt till knapp, textruta och text

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,7 +1,22 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import TextButton from "../components/TextButton";
+import { theme } from "../theme";
 
 export default function SignupPage() {
-    return (
-        <Box></Box>
-    );
-};
+  return (
+    <Box>
+      <Typography variant="h2">Register new user</Typography>
+      <Box
+        sx={{
+          width: "30rem",
+          height: "12rem",
+          marginTop: "5rem",
+          marginBottom: "2rem",
+          backgroundColor: theme.palette.secondary.main,
+          borderRadius: "35px",
+        }}
+      ></Box>
+      <TextButton mode="light">Register</TextButton>
+    </Box>
+  );
+}
