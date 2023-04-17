a77ac36408e54ee1fcfe614f7d431271663da374
JennyWeij <jennyweij@outlook.com>
2023-04-17 16:04:28 +0200

    centrerat texten och knappen

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -4,19 +4,31 @@ import { theme } from "../theme";
 
 export default function SignupPage() {
   return (
-    <Box>
-      <Typography variant="h2">Register new user</Typography>
-      <Box
-        sx={{
-          width: "30rem",
-          height: "12rem",
-          marginTop: "5rem",
-          marginBottom: "2rem",
-          backgroundColor: theme.palette.secondary.main,
-          borderRadius: "35px",
-        }}
-      ></Box>
-      <TextButton mode="light">Register</TextButton>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <Box sx={{ textAlign: "center" }}>
+        <Typography marginTop="5rem" variant="h2">
+          Register new user
+        </Typography>
+        <Box
+          sx={{
+            width: "30rem",
+            height: "12rem",
+            marginTop: "3rem",
+            marginBottom: "2rem",
+            backgroundColor: theme.palette.secondary.main,
+            borderRadius: "35px",
+            textAlign: "center",
+          }}
+        ></Box>
+        <TextButton mode="light">Register</TextButton>
+      </Box>
     </Box>
   );
 }
