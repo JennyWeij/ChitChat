11023b824c846a91d21232270a18057cd2a06f13
JennyWeij <jennyweij@outlook.com>
2023-04-17 16:42:53 +0200

    lagt till textfields för password och username

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, TextField, Typography } from "@mui/material";
 import TextButton from "../components/TextButton";
 import { theme } from "../theme";
 
@@ -9,24 +9,43 @@ export default function SignupPage() {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        height: "100vh",
+        marginTop: "4rem",
       }}
     >
       <Box sx={{ textAlign: "center" }}>
-        <Typography marginTop="5rem" variant="h2">
-          Register new user
-        </Typography>
+        <Typography variant="h2">Register new user</Typography>
         <Box
           sx={{
             width: "30rem",
-            height: "12rem",
-            marginTop: "3rem",
+            height: "13rem",
+            marginTop: "2rem",
             marginBottom: "2rem",
             backgroundColor: theme.palette.secondary.main,
             borderRadius: "35px",
             textAlign: "center",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "end",
           }}
-        ></Box>
+        >
+          <TextField
+            id="outlined-basic"
+            // label="Username"
+            variant="outlined"
+            sx={{
+              backgroundColor: "white",
+              marginTop: "2rem",
+              marginBottom: "2rem",
+              marginRight: "1.5rem",
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            // label="Password"
+            variant="outlined"
+            sx={{ backgroundColor: "white", marginRight: "1.5rem" }}
+          />
+        </Box>
         <TextButton mode="light">Register</TextButton>
       </Box>
     </Box>
