d19948f5ff49c9a0940626f53a497002b0883d4e
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-04-19 09:33:21 +0200

    form styling

--- client/src/components/CreatePostForm.tsx
+++ client/src/components/CreatePostForm.tsx
@@ -2,10 +2,12 @@ import {
     Box,
     FormControl,
     InputLabel,
-    TextField
+    TextField,
+    Typography,
 } from "@mui/material";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { theme } from "../theme";
 import TextButton from "./TextButton";
 
 const validationSchema = Yup.object().shape({
@@ -28,42 +30,58 @@ export default function CreatePostForm({ onSubmit }: Props) {
   return (
     <form onSubmit={formik.handleSubmit}>
       <Box sx={formContainer}>
-        <InputLabel htmlFor="content">Create a new post</InputLabel>
-        <FormControl
-          fullWidth
-          error={Boolean(formik.errors.content && formik.touched.content)}
-        >
-          <TextField
+        <InputLabel htmlFor="content">
+          <Typography variant="h2">Create a new post</Typography>
+        </InputLabel>
+        <Box sx={formBackground}>
+          <FormControl
             fullWidth
-            margin="normal"
-            variant="outlined"
-            id="content"
-            name="content"
-            label="Message"
-            multiline
-            rows={3}
-            value={formik.values.content}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            helperText={formik.touched.content && formik.errors.content}
             error={Boolean(formik.errors.content && formik.touched.content)}
-          />
-        </FormControl>
-      </Box>
-      <Box sx={buttonContainer}>
-      <TextButton mode="dark">Post</TextButton>
+          >
+            <TextField
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              id="content"
+              name="content"
+              multiline
+              rows={3}
+              value={formik.values.content}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              //helperText={formik.touched.content && formik.errors.content}
+              error={Boolean(formik.errors.content && formik.touched.content)}
+            />
+          </FormControl>
+          <Box sx={buttonContainer}>
+            <TextButton mode="dark">Post</TextButton>
+          </Box>
+        </Box>
       </Box>
     </form>
   );
 }
 
 const formContainer = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-}
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  marginTop: "2rem",
+};
 
 const buttonContainer = {
-    display: "flex",
-    justifyContent: "flex-end"
-}
+  display: "flex",
+  width: "100%",
+  justifyContent: "flex-end",
+};
+
+const formBackground = {
+  display: "flex",
+  flexDirection: "column",
+  width: "80%",
+  //gap: "2rem",
+  backgroundColor: theme.palette.secondary.main,
+  padding: "1rem 2rem",
+  borderRadius: "35px",
+  margin: "1rem"
+};
