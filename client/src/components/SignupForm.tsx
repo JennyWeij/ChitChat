604debf8e6bea7e6d7725d9abc046ee3bbb9c262
JennyWeij <jennyweij@outlook.com>
2023-04-20 14:35:48 +0200

    lagt stylingen längst ner i SignUp

---
+++ client/src/components/SignupForm.tsx
@@ -0,0 +1,150 @@
+// import { useNavigate } from "react-router-dom";
+// import * as Yup from "yup";
+// import { useAuth } from "../contexts/AuthContext";
+
+// interface FormValues {
+//   username: string;
+//   password: string;
+// }
+
+// const initialValues: FormValues = {
+//   username: "",
+//   password: "",
+// };
+
+// const validationSchema = Yup.object().shape({
+//   username: Yup.string().required("Username is required"),
+//   password: Yup.string()
+//     .min(8, "Password must be at least 8 characters")
+//     .required("Password is required"),
+// });
+
+// export default function SignUpForm() {
+//   const { login } = useAuth();
+//   const navigate = useNavigate();
+//   const handleSubmit = async (values: FormValues) => {
+//     console.log("Form data:", values);
+//     const success = await login(values.username, values.password);
+//     if (success) {
+//       navigate("/");
+//     }
+//   };
+
+//   return (
+//     <Box>
+
+//      <Box
+// sx={{
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: "center",
+//   marginTop: "4rem",
+// }}
+// >
+// <Box sx={{ textAlign: "center" }}>
+//   <Typography variant="h2">Register new user</Typography>
+//   <Box
+//     sx={{
+//       width: { xs: "20rem", sm: "25rem", md: "30rem", lg: "32rem" },
+//       height: "13rem",
+//       marginTop: "2rem",
+//       marginBottom: "2.5rem",
+//       backgroundColor: theme.palette.secondary.main,
+//       borderRadius: "35px",
+//       textAlign: "center",
+//       display: "grid",
+//       gridTemplateColumns: "auto 1fr",
+//       gridTemplateRows: {
+//         xs: "repeat(4, 1fr)",
+//         sm: "repeat(4, 1fr)",
+//         md: "repeat(2, 1fr)",
+//       },
+//       gap: "0.2rem",
+//       alignItems: "center",
+//       justifyItems: "start",
+//     }}
+//   >
+//     <Typography
+//       variant="h4"
+//       gridColumn="1"
+//       sx={{
+//         marginLeft: {
+//           xs: "4.5rem",
+//           sm: "7rem",
+//           md: "1.5rem",
+//           lg: "3rem",
+//         },
+//         marginTop: {
+//           xs: "0rem",
+//           sm: "0rem",
+//           md: "2rem",
+//           lg: "2rem",
+//         },
+//         gridRow: { xs: "1", sm: "1" },
+//       }}
+//     >
+//       Create username
+//     </Typography>
+//     <TextField
+//       id="outlined-basic"
+//       variant="outlined"
+//       sx={{
+//         backgroundColor: theme.palette.white.main,
+//         gridColumn: { xs: "1", sm: "1", md: "2" },
+//         gridRow: { xs: "2", sm: "2", md: "1" },
+//         marginRight: "1rem",
+//         marginTop: {
+//           xs: "0rem",
+//           sm: "0rem",
+//           md: "2rem",
+//           lg: "2rem",
+//         },
+//         marginLeft: {
+//           xs: "3rem",
+//           sm: "5.5rem",
+//           md: "1.5rem",
+//           lg: "0.5rem",
+//         },
+//       }}
+//     />
+//     <Typography
+//       variant="h4"
+//       gridColumn="1"
+//       sx={{
+//         marginLeft: {
+//           xs: "4.5rem",
+//           sm: "7rem",
+//           md: "1.5rem",
+//           lg: "3rem",
+//         },
+//         gridRow: { xs: "3", sm: "3" },
+//         marginBottom: { xs: "0rem", sm: "0rem", md: "2rem", lg: "2rem" },
+//       }}
+//     >
+//       Create password
+//     </Typography>
+//     <TextField
+//       id="outlined-basic"
+//       variant="outlined"
+//       sx={{
+//         backgroundColor: theme.palette.white.main,
+//         gridColumn: { xs: "1", sm: "1", md: "2" },
+//         gridRow: { xs: "4", sm: "4", md: "3" },
+//         marginRight: "1rem",
+//         marginBottom: { xs: "1rem", sm: "1rem", md: "2rem", lg: "2rem" },
+//         marginLeft: {
+//           xs: "3rem",
+//           sm: "5.5rem",
+//           md: "1.5rem",
+//           lg: "0.5rem",
+//         },
+//       }}
+//     />
+//   </Box>
+
+//   <TextButton mode="light">Register</TextButton>
+// </Box>
+// <TestForm />
+//     </Box>
+//   );
+//     };
