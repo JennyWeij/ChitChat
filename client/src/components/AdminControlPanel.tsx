4d478dd9b601176eaab916c9bf85ccb2142af841
Tara Skoglund <skoglundtara@gmail.com>
2023-04-21 15:30:27 +0200

    added underTitel

--- client/src/components/AdminControlPanel.tsx
+++ client/src/components/AdminControlPanel.tsx
@@ -1,36 +1,54 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 import { posts } from "../../data";
+import { themeAdmin } from "../theme";
 import AdminControlCard from "./AdminControlCard";
 
 export default function AdminControlPanel() {
   const adminUsers = posts.filter((post) => post.role === "admin");
   const regularUsers = posts.filter((post) => post.role === "user");
-  
+
   return (
-    <Box display="flex" justifyContent="center" flexDirection="column">
+    <Box sx={container}>
       <Box display="flex" justifyContent="center" marginBottom="3rem">
-        <Typography variant="h2" fontWeight="bold">
-          Admin
-        </Typography>
+        <Typography sx={adminTitle}>Admin</Typography>
       </Box>
+      <Typography sx={underTitel}>All Users</Typography>
+      <Divider sx={dividerStyling} />
 
       {/* Admin user */}
       {adminUsers.map((user, index) => (
-        <AdminControlCard
-          key={index}
-          name={user.name}
-          role={user.role}
-        />
+        <AdminControlCard key={index} name={user.name} role={user.role} />
       ))}
 
       {/* reguler user */}
       {regularUsers.map((user, index) => (
-        <AdminControlCard
-          key={index}
-          name={user.name}
-          role={user.role}
-        />
+        <AdminControlCard key={index} name={user.name} role={user.role} />
       ))}
     </Box>
   );
 }
+
+const container = {
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
+  alignItems: "center",
+};
+
+const adminTitle = {
+  fontWeight: "bold",
+  fontSize: "2rem",
+  color: themeAdmin.palette.darktext.main,
+  marginBottom: "2rem",
+};
+
+const dividerStyling = {
+  backgroundColor: themeAdmin.palette.mediumtext.main,
+  width: "80%",
+  margin: "1rem",
+};
+
+const underTitel = {
+  variant: "h2",
+  color: themeAdmin.palette.darktext.main,
+};
