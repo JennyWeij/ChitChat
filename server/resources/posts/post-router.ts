ba1a1ede430f9c42f5616fe864900db03d7bf1c3
MadeleineGustafson <madeleinegu@gmail.com>
2023-04-23 13:57:52 +0200

    create new post

--- server/resources/posts/post-router.ts
+++ server/resources/posts/post-router.ts
@@ -1,15 +1,27 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import PostModel from "./post-model";
 
-const postRouter = express
-  .Router()
-  .get("/api/posts", async (req, res) => {
-    const posts = await PostModel.find({});
-    res.json(posts);
-  })
-  .post("/api/posts", async (req, res) => {
-    const post = await PostModel.create(req.body);
-    res.status(201).json(post);
-  });
+const postRouter = express.Router();
+
+postRouter.get("/api/posts", async (req: Request, res: Response) => {
+  const posts = await PostModel.find({});
+  res.json(posts);
+});
+
+postRouter.post("/api/posts", async (req: Request, res: Response) => {
+  if (req.session && req.session.user._id) {
+    try {
+      const post = await PostModel.create({
+        ...req.body,
+        author: req.session.user._id,
+      });
+      res.status(201).json(post);
+    } catch (err) {
+      res.status(401).json({ message: "Could not create post" });
+    }
+  } else {
+    res.status(401).json({ message: "You must login to create a post" });
+  }
+});
 
 export default postRouter;
