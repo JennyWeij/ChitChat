db987df1db2e3a0687fded9b7260bf546442c674
MadeleineGustafson <madeleinegu@gmail.com>
2023-04-24 10:13:59 +0200

    must login to create post

--- server/resources/posts/post-model.ts
+++ server/resources/posts/post-model.ts
@@ -1,18 +1,29 @@
-import { InferSchemaType, Schema, SchemaTypes, model } from "mongoose";
+import { InferSchemaType, model, Schema, SchemaTypes } from "mongoose";
 
-const postSchema = new Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  author: { type: SchemaTypes.ObjectId, ref: 'user', required: true },
-},
-{
-  versionKey: false,
-  timestamps: true,
-}
+const postSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: SchemaTypes.ObjectId, ref: "User", required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
 );
+
+// Add a reference to the user schema
+postSchema.set("toObject", { virtuals: true });
+postSchema.set("toJSON", { virtuals: true });
+postSchema.virtual("user", {
+  ref: "User",
+  localField: "author",
+  foreignField: "_id",
+  justOne: true,
+});
 
 export type Post = InferSchemaType<typeof postSchema>;
 
-export const PostModel = model("post", postSchema);
+export const PostModel = model<Post>("Post", postSchema);
 
 export default PostModel;
--- server/resources/posts/post-router.ts
+++ server/resources/posts/post-router.ts
@@ -9,7 +9,7 @@ postRouter.get("/api/posts", async (req: Request, res: Response) => {
 });
 
 postRouter.post("/api/posts", async (req: Request, res: Response) => {
-  if (req.session && req.session.user._id) {
+  if (req.session && req.session.user && req.session.user._id) {
     try {
       const post = await PostModel.create({
         ...req.body,
@@ -20,8 +20,27 @@ postRouter.post("/api/posts", async (req: Request, res: Response) => {
       res.status(401).json({ message: "Could not create post" });
     }
   } else {
-    res.status(401).json({ message: "You must login to create a post" });
+    res
+      .status(401)
+      .json(JSON.stringify({ message: "You must login to create a post" }));
   }
 });
+
+// postRouter.get("/api/posts/:postId", async (req: Request, res: Response) => {
+//   const { postId } = req.params;
+
+//   try {
+//     const post = await PostModel.findOne({ _id: postId }).populate(
+//       "author",
+//       "username"
+//     );
+//     if (!post) {
+//       return res.status(404).json({ message: "Post not found" });
+//     }
+//     res.json(post);
+//   } catch (err) {
+//     res.status(500).json({ message: "Could not retrieve post" });
+//   }
+// });
 
 export default postRouter;
